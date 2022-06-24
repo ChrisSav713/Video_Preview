4f3f68f62e51d349cc155abc1eed774059184b9b
ChrisSav713 <106366587+ChrisSav713@users.noreply.github.com>
2022-06-24 17:47:51 -0400

    video added and functional

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("page loaded...");
+
+var vid = document.getElementById("video");
+vid.muted = true;
+vid.addEventListener("mouseover", videoStart);
+vid.addEventListener("mouseout", videoPause);
+
+function videoStart() {
+    vid.play();
+}
+
+function videoPause() {
+    vid.pause();
+}
